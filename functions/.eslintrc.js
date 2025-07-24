295f20f3923ea6093415512083c0ace270720386
s-sreedhar <raigotroy@gmail.com>
2025-07-24 20:59:48 +0530

    added firebase functions

---
+++ functions/.eslintrc.js
@@ -0,0 +1,31 @@
+import js from '@eslint/js';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js', '**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module'
+      },
+      globals: {
+        ...globals.node, // This includes Node.js globals like require, exports, console, etc.
+        node: true
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      'no-console': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+      'no-var': 'off',
+    }
+  }
+];
